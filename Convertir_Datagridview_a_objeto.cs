f7b0f3d2aa881a2bba20117a967ac9789616718c
Samuel Gomez <106413475+Dev-SamGomez@users.noreply.github.com>
2022-07-28 18:57:05 -0600

    Update Convertir_Datagridview_a_objeto.cs

--- Convertir_Datagridview_a_objeto.cs
+++ Convertir_Datagridview_a_objeto.cs
@@ -1,1 +1,1 @@
-var Result = dgvCortosCompletos.Rows.OfType<DataGridViewRow>().Select(r => r.Cells.OfType<DataGridViewCell>().Select(c => c.Value).ToArray()).ToList();
+var Result = TuDataGridView.Rows.OfType<DataGridViewRow>().Select(r => r.Cells.OfType<DataGridViewCell>().Select(c => c.Value).ToArray()).ToList();
